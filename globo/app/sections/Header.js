6c92f39c0f85f1aa9fd5c171a25dada21faeb9d5
Milind Chavan <milind.chavan@live.com>
2019-02-01 13:53:54 +0100

    With Basic Styling

--- globo/app/sections/Header.js
+++ globo/app/sections/Header.js
@@ -16,7 +16,26 @@ export class Header extends React.Component {
     render(){
         let display = this.state.isLoggedIn ? "Sample User" : this.props.message;
         return (
-            <Text onPress={this.toggleUser}>{display}</Text>
+            <View style={styles.headStyle}>
+                <Text 
+                    style={styles.headText}
+                    onPress={this.toggleUser}>{display}
+                </Text>
+            </View>
         );
     }
 }
+
+let styles = StyleSheet.create({
+    headText: {
+        textAlign : 'right',
+        color: '#ffffff',
+        fontSize: 20
+    },
+    headStyle:{
+        paddingTop:30,
+        paddingRight: 10,
+        paddingBottom: 10,
+        backgroundColor: '#35605a'
+    }
+});
--- globo/app/views/Home.js
+++ globo/app/views/Home.js
@@ -6,10 +6,10 @@ export class Home extends React.Component {
   render() {
     return (
       <View>
+        <Header message="Press to login"></Header>
         <Text>This is home components Line 1</Text>
         <Text>This is home components Line 2</Text>
         <Text>This is home components Line 3</Text>
-        <Header message="Press to login"></Header>
       </View>
     );
   }
